847e9534e2ea39b3d3ad707c6e1f1f8e11d8d192
Caleb Schappa <cschappa@yahoo.com>
2017-03-21 09:58:08 -0400

    checkpoint 34 - partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 require 'random_data'
 
+# Create topics
+
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentance,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 # Create posts
 50.times do
   Post.create!(
+    topic: topics.sample,
     title: RandomData.random_sentance,
     body: RandomData.random_paragraph
   )
@@ -17,5 +28,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
